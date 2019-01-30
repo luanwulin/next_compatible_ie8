b9cea2302de3b3e170c238fe18b8583cde43a2ae
linnanxuan <linnanxuan@zbj.com>
2019-01-30 17:52:52 +0800

    dist

---
+++ dist/lib/head.js
@@ -0,0 +1,125 @@
+'use strict';
+
+exports.__esModule = true;
+
+var _set = require('babel-runtime/core-js/set');
+
+var _set2 = _interopRequireDefault(_set);
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+exports.defaultHead = defaultHead;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _sideEffect = require('./side-effect');
+
+var _sideEffect2 = _interopRequireDefault(_sideEffect);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Head = function (_React$Component) {
+  (0, _inherits3.default)(Head, _React$Component);
+
+  function Head() {
+    (0, _classCallCheck3.default)(this, Head);
+    return (0, _possibleConstructorReturn3.default)(this, _React$Component.apply(this, arguments));
+  }
+
+  Head.prototype.render = function render() {
+    return null;
+  };
+
+  return Head;
+}(_react2.default.Component);
+
+Head.contextTypes = {
+  headManager: _propTypes2.default.object
+};
+function defaultHead() {
+  return [_react2.default.createElement('meta', { charSet: 'utf-8', className: 'next-head' })];
+}
+
+function reduceComponents(components) {
+  var _components$map$map$r;
+
+  return (_components$map$map$r = components.map(function (c) {
+    return c.props.children;
+  }).map(function (children) {
+    return _react2.default.Children.toArray(children);
+  }).reduce(function (a, b) {
+    return a.concat(b);
+  }, []).reverse()).concat.apply(_components$map$map$r, defaultHead()).filter(function (c) {
+    return !!c;
+  }).filter(unique()).reverse().map(function (c) {
+    var className = (c.className ? c.className + ' ' : '') + 'next-head';
+    return _react2.default.cloneElement(c, { className: className });
+  });
+}
+
+function mapOnServer(head) {
+  return head;
+}
+
+function onStateChange(head) {
+  if (this.context && this.context.headManager) {
+    this.context.headManager.updateHead(head);
+  }
+}
+
+var METATYPES = ['name', 'httpEquiv', 'charSet', 'itemProp', 'property'];
+
+// returns a function for filtering head child elements
+// which shouldn't be duplicated, like <title/>.
+
+function unique() {
+  var tags = new _set2.default();
+  var metaTypes = new _set2.default();
+  var metaCategories = {};
+
+  return function (h) {
+    switch (h.type) {
+      case 'title':
+      case 'base':
+        if (tags.has(h.type)) return false;
+        tags.add(h.type);
+        break;
+      case 'meta':
+        for (var i = 0, len = METATYPES.length; i < len; i++) {
+          var metatype = METATYPES[i];
+          if (!h.props.hasOwnProperty(metatype)) continue;
+
+          if (metatype === 'charSet') {
+            if (metaTypes.has(metatype)) return false;
+            metaTypes.add(metatype);
+          } else {
+            var category = h.props[metatype];
+            var categories = metaCategories[metatype] || new _set2.default();
+            if (categories.has(category)) return false;
+            categories.add(category);
+            metaCategories[metatype] = categories;
+          }
+        }
+        break;
+    }
+    return true;
+  };
+}
+
+exports.default = (0, _sideEffect2.default)(reduceComponents, onStateChange, mapOnServer)(Head);
---
+++ dist/lib/shallow-equals.js
@@ -0,0 +1,15 @@
+"use strict";
+
+exports.__esModule = true;
+exports.default = shallowEquals;
+function shallowEquals(a, b) {
+  for (var i in a) {
+    if (b[i] !== a[i]) return false;
+  }
+
+  for (var _i in b) {
+    if (b[_i] !== a[_i]) return false;
+  }
+
+  return true;
+}
