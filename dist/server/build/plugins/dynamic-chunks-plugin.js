54ce1f285c018d8429e9c9b3566b62d18720a2df
linnanxuan <linnanxuan@zbj.com>
2019-03-02 17:35:43 +0800

    remove eslint

---
+++ dist/server/build/plugins/dynamic-chunks-plugin.js
@@ -0,0 +1,70 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _webpackSources = require('webpack-sources');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var isImportChunk = /^chunks[/\\]/;
+var matchChunkName = /^chunks[/\\](.*)$/;
+
+var DynamicChunkTemplatePlugin = function () {
+  function DynamicChunkTemplatePlugin() {
+    (0, _classCallCheck3['default'])(this, DynamicChunkTemplatePlugin);
+  }
+
+  DynamicChunkTemplatePlugin.prototype.apply = function apply(chunkTemplate) {
+    chunkTemplate.plugin('render', function (modules, chunk) {
+      if (!isImportChunk.test(chunk.name)) {
+        return modules;
+      }
+
+      var chunkName = matchChunkName.exec(chunk.name)[1];
+      var source = new _webpackSources.ConcatSource();
+
+      source.add('\n        __NEXT_REGISTER_CHUNK(\'' + chunkName + '\', function() {\n      ');
+      source.add(modules);
+      source.add('\n        })\n      ');
+
+      return source;
+    });
+  };
+
+  return DynamicChunkTemplatePlugin;
+}();
+
+var DynamicChunksPlugin = function () {
+  function DynamicChunksPlugin() {
+    (0, _classCallCheck3['default'])(this, DynamicChunksPlugin);
+  }
+
+  DynamicChunksPlugin.prototype.apply = function apply(compiler) {
+    compiler.plugin('compilation', function (compilation) {
+      compilation.chunkTemplate.apply(new DynamicChunkTemplatePlugin());
+
+      compilation.plugin('additional-chunk-assets', function (chunks) {
+        chunks = chunks.filter(function (chunk) {
+          return isImportChunk.test(chunk.name) && compilation.assets[chunk.name];
+        });
+
+        chunks.forEach(function (chunk) {
+          // This is to support, webpack dynamic import support with HMR
+          var copyFilename = 'chunks/' + chunk.name;
+          compilation.additionalChunkAssets.push(copyFilename);
+          compilation.assets[copyFilename] = compilation.assets[chunk.name];
+        });
+      });
+    });
+  };
+
+  return DynamicChunksPlugin;
+}();
+
+exports['default'] = DynamicChunksPlugin;
---
+++ dist/server/build/plugins/pages-plugin.js
@@ -0,0 +1,69 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
+
+var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
+
+var _webpackSources = require('webpack-sources');
+
+var _utils = require('../../utils');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+var PageChunkTemplatePlugin = function () {
+  function PageChunkTemplatePlugin() {
+    (0, _classCallCheck3['default'])(this, PageChunkTemplatePlugin);
+  }
+
+  PageChunkTemplatePlugin.prototype.apply = function apply(chunkTemplate) {
+    chunkTemplate.plugin('render', function (modules, chunk) {
+      if (!_utils.IS_BUNDLED_PAGE.test(chunk.name)) {
+        return modules;
+      }
+
+      var routeName = _utils.MATCH_ROUTE_NAME.exec(chunk.name)[1];
+
+      // We need to convert \ into / when we are in windows
+      // to get the proper route name
+      // Here we need to do windows check because it's possible
+      // to have "\" in the filename in unix.
+      // Anyway if someone did that, he'll be having issues here.
+      // But that's something we cannot avoid.
+      if (/^win/.test(process.platform)) {
+        routeName = routeName.replace(/\\/g, '/');
+      }
+
+      routeName = '/' + routeName.replace(/(^|\/)index$/, '');
+
+      var source = new _webpackSources.ConcatSource();
+
+      source.add('\n        __NEXT_REGISTER_PAGE(\'' + routeName + '\', function() {\n          var comp = \n      ');
+      source.add(modules);
+      source.add('\n          return { page: comp.default }\n        })\n      ');
+
+      return source;
+    });
+  };
+
+  return PageChunkTemplatePlugin;
+}();
+
+var PagesPlugin = function () {
+  function PagesPlugin() {
+    (0, _classCallCheck3['default'])(this, PagesPlugin);
+  }
+
+  PagesPlugin.prototype.apply = function apply(compiler) {
+    compiler.plugin('compilation', function (compilation) {
+      compilation.chunkTemplate.apply(new PageChunkTemplatePlugin());
+    });
+  };
+
+  return PagesPlugin;
+}();
+
+exports['default'] = PagesPlugin;
