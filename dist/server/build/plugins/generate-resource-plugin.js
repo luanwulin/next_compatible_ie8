5bf10fabcd940520dc0528922a3977db26c2f665
linnanxuan <linnanxuan@zbj.com>
2019-03-06 01:11:55 +0800

    test plugin

--- dist/server/build/plugins/generate-resource-plugin.js
+++ dist/server/build/plugins/generate-resource-plugin.js
@@ -25,66 +25,68 @@ var GernerateResource = function () {
 
   GernerateResource.prototype.apply = function apply(compiler) {
     // 数据处理 用于生成 webpackMap
-    compiler.plugin('after-compile', function (compilation, callback) {
-      var pages = compilation.chunks.filter(function (chunk) {
-        return _utils.IS_BUNDLED_PAGE.test(chunk.name);
-      });
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
+        var pages = chunks.filter(function (chunk) {
+          return _utils.IS_BUNDLED_PAGE.test(chunk.name);
+        });
 
-      var webpackMap = {};
-      var destPath = compilation.options.output.path;
-      var destResourcePath = (0, _path.join)(destPath, 'resource');
+        var webpackMap = {};
+        var destPath = compilation.options.output.path;
+        var destResourcePath = (0, _path.join)(destPath, 'resource');
 
-      pages.forEach(function (chunk) {
-        var name = chunk.name;
+        pages.forEach(function (chunk) {
+          var name = chunk.name;
 
-        // 如果入口路径不包含 / 则不输出 例如 入口  name == 'project'
-        if (name.indexOf('/') < 0) {
-          return;
-        }
+          // 如果入口路径不包含 / 则不输出 例如 入口  name == 'project'
+          if (name.indexOf('/') < 0) {
+            return;
+          }
 
-        // 页面名
-        var rule = /^bundles[/\\]pages[/\\].*[/\\]index\.js$/;
-        if (rule.test(name)) {
-          name = name.replace(/[/\\]index\.js$/, '.js');
-        }
+          // 页面名
+          var rule = /^bundles[/\\]pages[/\\].*[/\\]index\.js$/;
+          if (rule.test(name)) {
+            name = name.replace(/[/\\]index\.js$/, '.js');
+          }
 
-        var pageName = name.replace(/bundles[/\\]pages[/\\](.*)[/\\]?\.js/, '$1');
+          var pageName = name.replace(/bundles[/\\]pages[/\\](.*)[/\\]?\.js/, '$1');
 
-        webpackMap[pageName] = {};
-        webpackMap[pageName].js = [];
-        webpackMap[pageName].css = [];
+          webpackMap[pageName] = {};
+          webpackMap[pageName].js = [];
+          webpackMap[pageName].css = [];
 
-        // 页面级别资源 (映射) 处理
-        [].concat(chunk.files).forEach(mapAsset);
+          // 页面级别资源 (映射) 处理
+          [].concat(chunk.files).forEach(mapAsset);
 
-        /**
-         * 根据资源类型，将其映射(map)到对应的数组中
-         * @param assetsPath  资源路径
-         */
-        function mapAsset(assetsPath) {
-          if (assetsPath) {
-            if ((0, _path.extname)(assetsPath) === '.js') {
-              // 绝对路径 = publicPath +  assetsPath
-              webpackMap[pageName].js.push(assetsPath);
-            } else if ((0, _path.extname)(assetsPath) === '.css') {
-              webpackMap[pageName].css.push(assetsPath);
+          /**
+           * 根据资源类型，将其映射(map)到对应的数组中
+           * @param assetsPath  资源路径
+           */
+          function mapAsset(assetsPath) {
+            if (assetsPath) {
+              if ((0, _path.extname)(assetsPath) === '.js') {
+                // 绝对路径 = publicPath +  assetsPath
+                webpackMap[pageName].js.push(assetsPath);
+              } else if ((0, _path.extname)(assetsPath) === '.css') {
+                webpackMap[pageName].css.push(assetsPath);
+              }
             }
           }
-        }
 
-        var newContent = (0, _stringify2['default'])(webpackMap);
-        // Replace the exisiting chunk with the new content
-        compilation.assets[(0, _path.join)(destResourcePath, 'resource.map.json')] = {
-          source: function source() {
-            return newContent;
-          },
-          size: function size() {
-            return newContent.length;
-          }
-        };
+          var newContent = (0, _stringify2['default'])(webpackMap);
+          // Replace the exisiting chunk with the new content
+          compilation.assets[(0, _path.join)(destResourcePath, 'resource.map.json')] = {
+            source: function source() {
+              return newContent;
+            },
+            size: function size() {
+              return newContent.length;
+            }
+          };
+        });
+
+        callback();
       });
-
-      callback();
     });
   };
 
--- server/build/plugins/generate-resource-plugin.js
+++ server/build/plugins/generate-resource-plugin.js
@@ -7,62 +7,63 @@ import {
 export default class GernerateResource {
   apply (compiler) {
     // 数据处理 用于生成 webpackMap
-    compiler.plugin('after-compile', function (compilation, callback) {
-      const pages = compilation
-        .chunks
-        .filter(chunk => IS_BUNDLED_PAGE.test(chunk.name))
+    compiler.plugin('compilation', function (compilation) {
+      compilation.plugin('optimize-chunk-assets', function (chunks, callback) {
+        const pages = chunks
+          .filter(chunk => IS_BUNDLED_PAGE.test(chunk.name))
 
-      const webpackMap = {}
-      const destPath = compilation.options.output.path
-      const destResourcePath = join(destPath, 'resource')
+        const webpackMap = {}
+        const destPath = compilation.options.output.path
+        const destResourcePath = join(destPath, 'resource')
 
-      pages.forEach((chunk) => {
-        let name = chunk.name
+        pages.forEach((chunk) => {
+          let name = chunk.name
 
-        // 如果入口路径不包含 / 则不输出 例如 入口  name == 'project'
-        if (name.indexOf('/') < 0) {
-          return
-        }
+          // 如果入口路径不包含 / 则不输出 例如 入口  name == 'project'
+          if (name.indexOf('/') < 0) {
+            return
+          }
 
-        // 页面名
-        const rule = /^bundles[/\\]pages[/\\].*[/\\]index\.js$/
-        if (rule.test(name)) {
-          name = name.replace(/[/\\]index\.js$/, `.js`)
-        }
+          // 页面名
+          const rule = /^bundles[/\\]pages[/\\].*[/\\]index\.js$/
+          if (rule.test(name)) {
+            name = name.replace(/[/\\]index\.js$/, `.js`)
+          }
 
-        const pageName = name.replace(/bundles[/\\]pages[/\\](.*)[/\\]?\.js/, `$1`)
+          const pageName = name.replace(/bundles[/\\]pages[/\\](.*)[/\\]?\.js/, `$1`)
 
-        webpackMap[pageName] = {}
-        webpackMap[pageName].js = []
-        webpackMap[pageName].css = [];
+          webpackMap[pageName] = {}
+          webpackMap[pageName].js = []
+          webpackMap[pageName].css = [];
 
-        // 页面级别资源 (映射) 处理
-        [].concat(chunk.files).forEach(mapAsset)
+          // 页面级别资源 (映射) 处理
+          [].concat(chunk.files).forEach(mapAsset)
 
-        /**
-         * 根据资源类型，将其映射(map)到对应的数组中
-         * @param assetsPath  资源路径
-         */
-        function mapAsset (assetsPath) {
-          if (assetsPath) {
-            if (extname(assetsPath) === '.js') {
-              // 绝对路径 = publicPath +  assetsPath
-              webpackMap[pageName].js.push(assetsPath)
-            } else if (extname(assetsPath) === '.css') {
-              webpackMap[pageName].css.push(assetsPath)
+          /**
+           * 根据资源类型，将其映射(map)到对应的数组中
+           * @param assetsPath  资源路径
+           */
+          function mapAsset (assetsPath) {
+            if (assetsPath) {
+              if (extname(assetsPath) === '.js') {
+                // 绝对路径 = publicPath +  assetsPath
+                webpackMap[pageName].js.push(assetsPath)
+              } else if (extname(assetsPath) === '.css') {
+                webpackMap[pageName].css.push(assetsPath)
+              }
             }
           }
-        }
 
-        const newContent = JSON.stringify(webpackMap)
-        // Replace the exisiting chunk with the new content
-        compilation.assets[join(destResourcePath, 'resource.map.json')] = {
-          source: () => newContent,
-          size: () => newContent.length
-        }
+          const newContent = JSON.stringify(webpackMap)
+          // Replace the exisiting chunk with the new content
+          compilation.assets[join(destResourcePath, 'resource.map.json')] = {
+            source: () => newContent,
+            size: () => newContent.length
+          }
+        })
+
+        callback()
       })
-
-      callback()
     })
   }
 }
